1ad74f2f6653684fc7e8ba132564fe31f5816efd
Igor Almeida <igor.almeida@evtit.com>
2024-04-15 11:44:58 -0300

    remove location path

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/global";
 import { Home } from "./pages/home";
 import { GiftsPage } from "./pages/gifts";
-import { Location } from "./pages/location";
+// import { Location } from "./pages/location";
 import Root from "./components/BaseKit";
 import { TipsPage } from "./pages/tips";
 import { ThemeProvider } from "styled-components";
 import { Themes } from "./styles/palette";
+import { CheckoutPage } from "./pages/payment/checkout";
+import { PaymentProvider } from "./context/provider/payment";
 
 const router = createBrowserRouter([
   {
@@ -25,9 +27,13 @@ const router = createBrowserRouter([
         path: "/Tips",
         element: <TipsPage />
       },
+      // {
+      //   path: "/location",
+      //   element: <Location />
+      // },
       {
-        path: "/location",
-        element: <Location />
+        path: "/payment",
+        element: <CheckoutPage />
       },
     ],
   }]);
@@ -35,8 +41,10 @@ function App() {
   return (
     <div>
       <ThemeProvider theme={Themes}>
-        <GlobalStyle />
-        <RouterProvider router={router} />
+        <PaymentProvider>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </PaymentProvider>
       </ThemeProvider>
     </div>
   );
