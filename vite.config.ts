09824ced66dcf39bc4489f1b2d0b1bd3e1a19d82
Igor Almeida <igor.almeida@evtit.com>
2024-06-09 00:35:55 -0300

    fix: pwa build setup to install app

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
-import { VitePWA } from "vite-plugin-pwa";
+import { ManifestOptions, VitePWA } from "vite-plugin-pwa";
+import manifestsSetup from './public/manifest.json'
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '',
+  root: '.',
+  base: '/',
   plugins: [
     react(),
     viteTsconfigPaths(),
     VitePWA({
       registerType: 'autoUpdate',
-      manifest: {
-        short_name: "Igor e Maria",
-        name: "Nosso casamento!",
-        icons: [
-          {
-            src: "./public/icon.png",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/x-icon"
-          },
-          {
-            src: "./public/icon.png",
-            type: "image/png",
-            sizes: "192x192"
-          },
-          {
-            src: "./public/icon.png",
-            type: "image/png",
-            sizes: "512x512"
-          }
-        ],
-        start_url: ".",
-        display: "standalone",
-        theme_color: "#000000",
-        background_color: "#ffffff",
-      }
-
+      injectRegister: 'auto',
+      manifest: manifestsSetup as ManifestOptions
     })
   ],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
-  server: {
-    open: true,
-    port: 3000,
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+});
