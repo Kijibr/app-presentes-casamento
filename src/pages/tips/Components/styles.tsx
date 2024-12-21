33f70db9d7c91cff4711ee128eff68558a69de6a
Igor Almeida <igor.almeida@evtit.com>
2024-12-21 00:24:43 -0300

    update tips page

--- src/pages/tips/Components/styles.tsx
+++ src/pages/tips/Components/styles.tsx
@@ -1,19 +1,47 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Tips = styled.li`
-  font-size: 24px;
-  color: rgb(224, 174, 66);
-  padding-left: 24px;
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
+
+const Tips = styled.ul<{ index: number }>`
+  font-size: 24px;
+  background-color: ${props => props.theme.white};
+  color: gray;
+  padding-left: 24px;
+  padding: 12px 4px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);
+  border: 0px gray solid;
+  border-radius: 4px;
+
+  display: flex;
+  align-items: center;
+
+  opacity: 0;
+  transform: translateY(50px);
+
+  animation: ${fadeInUp} 0.5s ease forwards;
+  animation-delay: ${({ index }) => `${index * 0.1}s`};
+  transition: transform 0.2s ease;
+`;
+
 const Paper = styled.div`
   margin: 48px;
   padding: 24px;
-  background-color: #828e51;
+  background-color: #9ca771;
 
-  border: 2px solid orange;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   border-radius: 30px;
   
-  overflow: hidden;
+  overflow-x: hidden;
+  overflow-y: scroll;
 `;
 
 const Wrapper = styled.div`
@@ -23,9 +51,15 @@ const Wrapper = styled.div`
 `;
 
 const Title = styled.span`
-  font-size: 48px;
+  font-size: 36px;
   align-self: center;
-  
+  display: flex;
+  align-items: center;
+  color: ${props => props.theme.white};
 `;
 
-export { Paper, Wrapper, Title, Tips }
+const Subtitle = styled(Title)`
+  font-size: 32px;
+`;
+
+export { Paper, Wrapper, Title, Subtitle, Tips }
--- src/pages/tips/index.tsx
+++ src/pages/tips/index.tsx
@@ -1,5 +1,18 @@
 import { Content } from "../../styles/style";
-import { Tips, Wrapper, Title, Paper } from "./Components/styles";
+import { Tips, Wrapper, Title, Paper, Subtitle } from "./Components/styles";
+import { Flower } from 'lucide-react';
+
+const generalTips = [
+  'No calor os mosquitos costumam atacar, não esqueça o repelente.',
+  'Optamos pelo traje esporte fino.',
+  'Distribuiremos leques para que possam se refrescar caso tenha calor durante o casamento.'
+];
+
+const womenTips = [
+  'Na hora de escolherem seus looks deem preferêcia para vestidos.',
+  'O local possui bastante vegetação e área coberta, porém a cerimônia será ao ar livre, recomendamos calçados de sola ampla, evitando saltos finos.',
+  'A cerimônia será na grama, então tome cuidado com saltos finos.',
+];
 
 export function TipsPage() {
 
@@ -7,16 +20,27 @@ export function TipsPage() {
     <Content>
       <Paper>
         <Wrapper>
-          <Title>Aqui vão algumas dicas para o dia do casamento!</Title>
-          <Tips>Procurem não utilizar roupas muito pesadas, estaremos no verão.</Tips>
-          <Tips>Cheguem cedo, terá uma mesa de boas-vindas aguardando a presença de todos!</Tips>
-          <Tips>As fotos com o casal ocorrerão na própria mesa dos convidados</Tips>
-          
-          <Tips>Recomendamos trajes de tons mais claros para que se sintam mais confortável em relação ao clima;</Tips>
-          <br />
-          <Tips>Para as mulheres:</Tips>
-          <Tips>O local possui bastante vegetação e área coberta, porém a cerimônia será ao ar livre, recomendamos calçados de sola ampla, evitando saltos finos;</Tips>
-          <Tips>Distribuiremos leques para que possam se refrescar caso tenha calor durante o casamento;</Tips>
+          <Title>
+            <Flower size={32} color="white" />
+            Anote essas dicas para que tudo corra bem e você esteja super confortável no grande dia
+            <Flower size={32} color="white" />
+          </Title>
+          {generalTips.map((item, index) =>
+            <Tips index={index} >
+              <Flower size={32} />
+              {item}
+            </Tips>
+          )}
+
+          <Subtitle>
+            Para as mulheres
+          </Subtitle>
+          {womenTips.map((item, index) =>
+            <Tips index={index} >
+              <Flower size={32} />
+              {item}
+            </Tips>
+          )}
         </Wrapper>
       </Paper>
     </Content>
